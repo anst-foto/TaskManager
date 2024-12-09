86aee69323569b7f4d3d7932d72f5bf362887f1d
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-09 20:56:35 +0300

    Initial commit

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskManager.Models;
+
+public enum TaskPriority
+{
+    Low, Medium, High
+}
+
+public class Task
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateEnd{ get; set; }
+    public TaskPriority Priority { get; set; }
+}
